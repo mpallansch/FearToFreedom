9f7a591a71aef4e56daa36cf0b40f9b843a82d4d
mpallansch <matthew.pallansch@gmail.com>
2024-06-18 18:38:41 -0400

    Adds Basic Movement and Collision

---
+++ Player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+
+const SPEED = 1000.0
+
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * SPEED
+
+	move_and_slide()
